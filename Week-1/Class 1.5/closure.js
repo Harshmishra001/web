0bdc6281235416b5c08818c286399206d6906bf6
Harshmishra001 <iconharsh01@gmail.com>
2024-11-29 20:53:47 +0530

    week 1

---
+++ Week-1/Class 1.5/closure.js
@@ -0,0 +1,21 @@
+function add() {
+  let counter = 0;
+  console.log(counter)  // 0
+
+  function plus() {
+      counter += 1
+      var a=9
+      console.log(counter)  // 1 
+  }
+
+  plus();  
+
+  console.log(counter)   // 1 
+  console.log(a) // shows error a is not defined outside plus() no matter of var, let or const
+}
+
+add()
+
+// A closure is the combination of a function bundled together (enclosed) with references to its surrounding state 
+// (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner 
+// function. In JavaScript, closures are created every time a function is created, at function creation time.
